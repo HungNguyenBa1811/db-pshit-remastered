cbccfb6a857ceefcf1efba35fd7e9337594c5521
HungNguyenBa1811 <nahh1811@gmail.com>
2025-12-07 01:15:07 +0700

    feat(config): add Vercel configuration for API rewrites

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-console.log(">>> VITE CONFIG LOADED");
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://dbapi.ptit.edu.vn',
-        changeOrigin: true,
-        secure: false,
-      },
+    plugins: [react()],
+    server: {
+        proxy: {
+            '/api': {
+                target: 'https://dbapi.ptit.edu.vn',
+                changeOrigin: true,
+                secure: false,
+            },
+        },
     },
-  },
-})
+});
